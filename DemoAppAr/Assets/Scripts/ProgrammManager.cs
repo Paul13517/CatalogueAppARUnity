ed57f2f1a0c6befd767722fa81c4e5481598c3b5
veza-g <78474860+veza-g@users.noreply.github.com>
2021-12-22 11:23:23 +0300

    Download
    Download

---
+++ DemoAppAr/Assets/Scripts/ProgrammManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ProgrammManager : MonoBehaviour
+{
+    public GameObject PlaneMarkerPrefab;
+
+    private ARRaycastManager ARRaycastManagerScript;
+
+    private Vector2 TouchPosition;
+
+    public GameObject ObjectToSpawn;
+    void Start()
+    {
+        ARRaycastManagerScript = FindObjectOfType<ARRaycastManager>();
+
+        PlaneMarkerPrefab.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       ShowMarkerAndSetObject();
+    }
+
+    void ShowMarkerAndSetObject()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        ARRaycastManagerScript.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // show marker
+        if (hits.Count > 0)
+        {
+            PlaneMarkerPrefab.transform.position = hits[0].pose.position;
+            PlaneMarkerPrefab.SetActive(true);
+        }
+       // set object
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Instantiate(ObjectToSpawn, hits[0].pose.position, ObjectToSpawn.transform.rotation);
+            PlaneMarkerPrefab.SetActive(false);
+        }
+    }
+    
+
+    
+}
